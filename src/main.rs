7218f598825c5e5b8a7d67d5e70d7823b44551f1
Slinet6056 <inbox@slinet.me>
2025-05-08 20:29:23 +0800

    Create functions

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@
 // 注释！
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
